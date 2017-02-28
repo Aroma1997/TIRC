a4e3868f6f4edef663d45e3f90e00c11ef0e232a
Aroma1997 <aroma1997@aroma1997.org>
2017-02-28 13:02:17 +0100

    Added basic whois functionality.

--- src/main/java/aroma1997/tirc/UserToTwitchConnection.java
+++ src/main/java/aroma1997/tirc/UserToTwitchConnection.java
@@ -8,12 +8,21 @@ public class UserToTwitchConnection extends SingleConnection {
 		super(conn, Side.USER);
 	}
 	
+	@Override
 	protected void processLine(String line) throws IOException {
 		int spaceIndex = line.indexOf(' ');
 		String identifier = line.substring(0, spaceIndex);
 		String message = line.substring(spaceIndex + 1);
 		switch (identifier) {
 		case "WHO": //Do not relay WHO command, because Twitch doesn't support that.
+			break;
+		case "WHOIS":
+			SingleConnection other = conn.getRelayFor(getSide().getOpposite());
+			other.send(":" + getUserDomain(conn.user, true) + " 319 " + conn.user + " " + message + " :Unknown. Functionality not supported."); // Channels that person is in.
+			other.send(":" + getUserDomain(conn.user, true) + " 312 " + conn.user + " " + message + " " + conn.getSocketFor(Side.TWITCH).getInetAddress().getHostName()); //Connected server
+			other.send(":" + getUserDomain(conn.user, true) + " 311 " + conn.user + " " + message + " ~" + message + " " + getUserDomain(message, false) + " * " + message); //Username and client name.
+			other.send(":" + getUserDomain(conn.user, true) + " 318 " + conn.user + " " + message + " :End of /WHOIS list.");
+			other.writer.flush();
 			break;
 		case "NICK":
 			conn.user = message;
@@ -24,6 +33,14 @@ public class UserToTwitchConnection extends SingleConnection {
 			line = identifier + " " + message.toLowerCase();
 		default:
 			send(line);
+		}
+	}
+	
+	private String getUserDomain(String user, boolean actual) {
+		if (!actual && conn.user.equals(user)) {
+			return conn.getSocketFor(Side.USER).getInetAddress().getHostName();
+		} else {
+			return user + ".tmi.twitch.tv";
 		}
 	}
 }
